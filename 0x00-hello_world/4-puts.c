707783e01ff077087d3d9c0368226994100d8e84
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-14 04:32:20 +0300

    Hello C-lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(void)
 {
-  char hello[]="Programming is like building a multilingual puzzle";
+  char hello[]="\"Programming is like building a multilingual puzzle";
   puts(hello);
   return 0;
 }
