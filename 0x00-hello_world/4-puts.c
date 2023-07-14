1aa3bbdbe793acadb65341b91fc131b1345800e4
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-14 04:58:44 +0300

    Hello C-lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/*Puts example
+ * puts function insert a new line in the end and its like printf
+*/
+
+
 int main(void)
 {
 char hello[] = "\"Programming is like building a multilingual puzzle";
