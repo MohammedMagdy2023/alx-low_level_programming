58b623caff4929fec6b1b3af505f71db31b696c9
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-14 05:11:07 +0300

    Hello C-lang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/*This is an example on printf*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
