e3441444033d491146d206d2fb50aeff2bcac72f
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-15 03:23:30 +0300

    Hello C-lang

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <time.h>
 /* more headers goes there */
 /**
-main function telles if the n variable positive or nigative or zero
+* main function telles if the n variable positive or nigative or zero
 */
 /* betty style doc for function main goes there */
 int main(void)
