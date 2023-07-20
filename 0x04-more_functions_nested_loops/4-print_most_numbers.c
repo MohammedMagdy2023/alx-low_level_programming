b62e1c5f650bc2c50b29dd33530be971899b8bb8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-21 01:29:06 +0300

    C - Functions, nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int i = 48;
+for (i = 48; i < 58; i++)
+{
+if (i != 50 || i != 50){
+_putchar(i);
+} 
+}
+_putchar(10);
+}
