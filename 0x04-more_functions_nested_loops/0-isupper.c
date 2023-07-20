494f426591491045c2e32f8a3173175df572a6cf
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-20 23:16:55 +0300

    C - Functions, nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-*_islower - controls if a character is in lowercase
-*@c: character to be verified
-*Return: return 0 or 1
+* main 
+* checks the code and return 1 if uppercase
+* and 0 if lowercase
 */
 int _isupper(int c)
 {
