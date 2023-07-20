4b988c00ea6baaac96d95af6caf17d4665a3bc40
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-21 00:05:23 +0300

    C - Functions, nested loops

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* mul - function to multiply two number
-* @a : the first number
-* @b : the second number
+*mul - function to multiply two number
+*@a : the first number
+*@b : the second number
 *Return: return a * b
 */
 
