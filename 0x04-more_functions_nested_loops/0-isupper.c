c2e2b68586d2188140716f8eeec19eb43296b6f6
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-20 23:12:25 +0300

    C - Functions, nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main 
+ * checks the code and return 1 if uppercase
+ * and 0 if lowercase
+ * 
+*/
+
+int main(char c){
+
+if (c ){
+_putchar("%c: 1",c);
+}
+else
+{
+_putchar("%c: 0",c);
+}
+}
