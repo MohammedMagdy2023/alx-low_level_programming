9ea896c94145456f62a9419a3dc9b7012af20748
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-21 03:53:44 +0300

    C - Functions, nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+
+int more_numbers(void)
+{
+int j;
+int i;
+for (j = 0; j <= 10; j++)
+{
+for (i = 0; i < 15; i++ )
+{
+    _putchar(i);
+}
+_putchar("\n");
+}
+return (0);
+}
