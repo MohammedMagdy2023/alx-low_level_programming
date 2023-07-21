a194def40b513a2ee4f62a01c1d8d4dafef37c56
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-21 04:09:16 +0300

    C - Functions, nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,16 +4,21 @@
 *
 *Return: returns nothing
 */
-
 void more_numbers(void)
 {
-int j, i;
-for (j = 0; j < 10; j++)
+int number1, number2;
+
+for (number1 = 0; number1 < 10; number1++)
 {
-for (i = 0; i <= 14; i++ )
+for (number2 = 0; number2 <= 14; number2++)
+
 {
-_putchar(i);
+if (number2 > 9)
+{
+_putchar((number2 / 10) + '0');
 }
+_putchar((number2 % 10) + '0');
 }
 _putchar(10);
+}
 }
