76d14efc8bdd02e9f0da46e25629e3b827544559
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-25 01:35:06 +0300

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_reset_to_98 -  takes a pointer to an int as parameter 
+*_reset_to_98 - takes a pointer to an int as parameter 
 *and updates the value it points to to 98
 *@n: character to be verified
 *Return: return 0 or 1
