edc04951b8bd3847e3435b8dabb42d405484995d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-07-25 02:05:58 +0300

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
-*_reset_to_98 -  takes a pointer to an int as parameter and updates the value it points to to 98
-*@n: character to be verified
-*Return: return 0 or 1
+*swap_int - swap two integers
+*@a: first integer
+*@b: second integer
+*Return: nothing
 */
+
 void swap_int(int *a, int *b)
 {
 *a, *b = *b, *a;
