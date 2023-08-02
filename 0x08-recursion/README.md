7ff640b5ef29dd3e762542665a74a09e033737a8
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-02 22:22:48 +0300

    Recursion

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+#This is a new technology to learn (the recursion)
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+*void prototypes(void)
+*int prototypes(int)
+*char prototypes(char)
+*/
+
+void _puts_recursion(char *s);
+
+
+
+#endif /* MAIN_H */
