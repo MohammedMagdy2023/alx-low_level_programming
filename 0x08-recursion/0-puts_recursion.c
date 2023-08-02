e692b71384bd3295eede26bb496222b181894395
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-02 22:32:08 +0300

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
 *Return(0)
 */
 
-void _puts_recursion( char *s )
+void _puts_recursion(char *s)
 {
-if( *s )
+if(*s)
 {
-_putchar( *s );
-_puts_recursion( s + 1 );
+_putchar(*s);
+_puts_recursion(s + 1);
 }
 else
 {
