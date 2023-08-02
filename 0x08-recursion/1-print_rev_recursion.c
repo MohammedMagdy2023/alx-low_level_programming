fb10bdc953338eff4d9f15b0fca085fee6820409
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-02 22:37:11 +0300

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
 #include "main.h"
+/**
+* _print_rev_recursion: writes sting in reverse order 
+* @s:string
+* Return:void
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
