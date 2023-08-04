abd7f53fda5c94a723e4a57efa25e920f7947019
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-05 02:09:57 +0300

    C-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,13 @@
 
 /**
 * main - prints program name
-* @argc: number of arguments
-* @argv: array of arguments
+*@argc: number of arguments
+*@argv: array of arguments
 *Return: 0
 */
-int main(int __attribute__((unused)) argc, char *argv[]){
-printf("%s\n", argv[0]);
-return(0);
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
