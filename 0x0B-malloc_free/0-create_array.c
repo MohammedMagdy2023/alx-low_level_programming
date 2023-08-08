fcf9776c9b3f8d099afb67ccda22b54c9a802bc4
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-09 00:52:02 +0300

    C memory handling (malloc, free)

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* create_array - creates an array of chars, and
+* initializes it with a specific char.
+*@size: Size of the array
+*@c: Character to insert
+*Return: NULL if size is zero or if it fails,
+*pointer to array if everything is normal.
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(sizeof(char) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		ptr[index] = c;
+
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _strdup - returns a pointer to a newly allocated
+*space in memory, which contains a copy of the
+*string given as a parameter.
+*@str:String to be copied
+*
+*Return: NULL in case of error, pointer to allocated
+*space
+*/
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int index, len;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+		len++;
+	ptr = malloc(sizeof(char) * (len + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (index = 0; str[index]; index++)
+	{
+		ptr[index] = str[index];
+	}
+
+	ptr[len] = '\0';
+
+	return (ptr);
+
+}
