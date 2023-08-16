8fd3c7c1085f4b344490dc108c5feb39d8d46a1d
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-16 23:52:02 +0300

    C Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,12 @@
 *@f: function which print name
 */
 
-void print_name(char *name, void (*f)(char *)){
-if(name && f)
-    f(name);
+void print_name(char *name, void (*f)(char *))
+{
+
+if (name == NULL || f == NULL)
+return;
+
+f(name);
+
 }
