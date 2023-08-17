0d1ee94d0a7e2fa2d57ca86665bfde41e65a74d6
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-17 14:19:18 -0700

    C-variadic_function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all():a funtion that calculate given arguments 
+ * return 0 if arguments number is 0
+*/
+
+int sum_them_all(const unsigned int n, ...){
+  if (n == 0){
+    return 0;
+  }else{
+    va_list arg_ptr;
+    va_start(arg_ptr, n);
+    va_arg(arg_ptr, int);
+    int result, i;
+    for (i == 0; i < n-1; i++){
+      result += va_arg(arg_ptr, int);
+    }
+    va_end(arg_ptr);
+  }
+}
