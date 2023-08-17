f15675d889b19bc1bc12e1a985dd5cdad4389677
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-17 15:41:53 -0700

    variadic functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - prints numbers followed by a new line
+*@separator: the string between characters
+*@n: the number of integers passed to function
+*Return:nothing 
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  va_list arg_ptr;
+  
+
+
+
+
+
+
+  
+}
