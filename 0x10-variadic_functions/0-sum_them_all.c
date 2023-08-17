9434742962178a0eb8d4d4603489289d5030b12b
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-17 14:27:36 -0700

    C-variadic_function

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,12 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all():a funtion that calculate given arguments 
- * return 0 if arguments number is 0
+*sum_them_all():a funtion that calculate given arguments 
+*return 0 if arguments number is 0
 */
 
-int sum_them_all(const unsigned int n, ...){
+int sum_them_all(const unsigned int n, ...)
+{
   if (n == 0){
     return 0;
   }else{
