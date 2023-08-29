9e7ebf6cd9682c1d4e4533f6da94393d8f1cfa38
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-08-30 01:17:34 +0300

    Linked List

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,12 @@
 #include "lists.h"
 
 /**
-*print_listint - a function that prints all elements of a list
-*
-*@h: pointer to first node
-*
-*Return: the number of nodes
+ * print_listint - a function that prints all elements of a list
+ *
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
 */
-
 size_t print_listint(const listint_t *h)
 {
 size_t node_count = 1;
