f4f66c13f0ccc9de487f3cd65c43c3567ea804ee
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-09-03 21:12:26 +0300

    Print Alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
-/***
- *main - prints alphabet in uppercase followed by a new line
+/**
+ *main - prints the lowercase alphabet excipt e and q
  *
- *Return:0
+ *Return: 0
  */
-
 int main(void)
-{       
-        int i;
+{
+	int i;
 
-        for (i = 97; i < 122; i++)
-        {
-            if (i == 101 || i == 113){
-                continue;
-            }else{
-                putchar(i);
-            }
-        }
-        /*Print alphabet in uppercase*/
-        putchar('\n');
-        return(0);
+	for (i = 97; i <= 122; i++)
+	{
+		if (i == 101 || i == 113)
+		{
+			continue;
+		}else
+		{
+			putchar(i);
+		}
+	}
+	return (0);
 }
 
