cb78be584b1862d7c00dd81d4385981a1c810e46
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-09-05 00:01:10 +0300

    FILE I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,29 @@
 #include "main.h"
 
 /**
+ *read_textfile - read a text file and print it to stout
  *
+ *@filename: pointer to file 
+ *@letters: number os bytes 
  *
- *
- *
- * /
+ *Return: number of bytes
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f;
+	ssize_t byte;
+	char buffer[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	f = open(filename, O_RDONLY);
+	if (f == -1)
+		return (0);
+	byte = read(f, &buffer[0], letters);
+	byte = write(STDOUT_FILENO, &buffer[0], byte);
+	close(f);
+
+	return (byte);
+}
+
