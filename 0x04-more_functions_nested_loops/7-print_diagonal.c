88fc2029ba47933e8872fa6ebef91383e242e702
MohammedMagdy2023 <128869362+MohammedMagdy2023@users.noreply.github.com>
2023-09-10 23:45:56 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,23 +4,31 @@
 *@n:parameter
 *Return:returns nothing
 */
+
 void print_diagonal(int n)
 {
-int len, space;
+int i, j;
 
-if (n > 0)
+if (n <= 0 )
+_putchar('\n');
+else
 {
-for (len = 0; len < n; len++)
+for (i = 0; i <= n; i++ ){
+if (i == 0){
+_putchar(92);
+_putchar('\n');
+}else
 {
-for (space = 0; space < len; space++)
+j = 0;
+while (j < i){
 _putchar(' ');
-
-_putchar('\\');
-
-if (len == (n - 1))
-continue;
+j++;
+}
+_putchar(92);
 _putchar('\n');
 }
+
 }
 _putchar('\n');
+}
 }
