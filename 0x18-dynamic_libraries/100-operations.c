634d1623ef0db068445d8cddf49faf8eae60d9af
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2023-10-16 14:56:16 -0700

    C & PYTHON

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 
-int add(int a, int b)
-{
-return a + b;
+int add(int a, int b) {
+	return a + b;
 }
-int sub(int a, int b)
-{
-return a - b;
+
+int sub(int a, int b) {
+	return a - b;
 }
-int mul(int a, int b)
-{
-return a * b;
+
+int mul(int a, int b) {
+	return a * b;
 }
-int div(int a, int b)
-{
-if (b == 0)
-{
-printf("Error: division by 0\n");
-return (0);
+
+int div(int a, int b) {
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+	return a / b;
 }
-return a / b;
-}
-int mod(int a, int b)
-{
-if (b == 0)
-{
-printf("Error: division by 0\n");	
-return (0);
-}
-return a / b;
+
+int mod(int a, int b) {
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");	
+		return (0);
+	}
+	return a % b;
 }
