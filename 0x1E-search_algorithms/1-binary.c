37b34b8647d574b978dfe80cde47c1f0ffa2e2e7
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-07 11:07:36 -0800

    search algos

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,26 +9,48 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-size_t mid, lower, upper;
+size_t upper = size - 1, lower = 0, mid;
 
 if (array == NULL)
 return (-1);
 
-for (lower = 0, upper = size - 1; upper >= lower;)
+while (lower < upper)
 {
-printf("Searching in array: ");
-for (mid = lower; mid < upper; mid++)
-printf("%d, ", array[mid]);
-printf("%d\n", array[mid]);
-
-mid = lower + (upper - lower) / 2;
-if (array[mid] == value)
-return (mid);
-if (array[mid] > value)
-upper = mid - 1;
-else
+mid = (upper + lower) / 2;
+print_array(array, lower, upper);
+if (array[mid] < value)
+{
 lower = mid + 1;
+}
+else if (array[mid] > value)
+{
+upper = mid - 1;
+}
+else
+{
+return (mid);
+}
 }
 
 return (-1);
+
+}
+
+/**                                                                             
+* print_array - prints all elements in array                                    
+* @array: the array to search in it                                                                         
+* @start: the beginning of the print range
+* @end: the end of the print range
+*/
+
+void print_array(int *array, size_t start, size_t end)
+{
+size_t i;
+
+printf("Searching in array: ");
+for (i = start; i < end; i++)
+{
+printf("%d, ", array[i]);
+}
+printf("%d\n", array[end]);
 }
