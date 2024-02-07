415b04acb0bb3bc3d7e0f07e297adbf6dfb84fd1
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-02-07 10:52:04 -0800

    search algos

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,10 +1,10 @@
 #include "search_algos.h"
 /**
- * binary_search - performs a binary search algorithm to find data
- * @array: the array to search in it
- * @size: the size of the array
- * @value: the value to search
- * Return: the value the position of the element of -1 if not found
+* binary_search - performs a binary search algorithm to find data
+* @array: the array to search in it
+* @size: the size of the array
+* @value: the value to search
+* Return: the value the position of the element of -1 if not found
 */
 
 int binary_search(int *array, size_t size, int value)
@@ -14,15 +14,11 @@ size_t upper = size - 1, lower = 0, mid;
 if (array == NULL)
 return (-1);
 
-do {
+while (lower < upper)
+{
 mid = (upper + lower) / 2;
 print_array(array, lower, upper);
-if (array[mid] == value)
-{
-return (mid);
-break;
-}
-else if (array[mid] < value)
+if (array[mid] < value)
 {
 lower = mid + 1;
 }
@@ -30,16 +26,21 @@ else if (array[mid] > value)
 {
 upper = mid - 1;
 }
-} while (lower < upper);
-
-return (-1);
+else
+{
+return (mid);
+}
 }
 
-/**
- * print_array - prints all elements in array
- * @array: the array to search in it
- * @start: the beginning of the print range
- * @end: the end of the print range
+return (-1);
+
+}
+
+/**                                                                             
+* print_array - prints all elements in array                                    
+* @array: the array to search in it                                                                         
+* @start: the beginning of the print range
+* @end: the end of the print range
 */
 
 void print_array(int *array, size_t start, size_t end)
